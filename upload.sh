fcab886ba17f8d969d5f8363863a86cf47d1a44f
flak <flak.yar@gmail.com>
2020-07-03 08:48:27 +0400

    update

--- upload.sh
+++ upload.sh
@@ -1,9 +1,9 @@
-echo "<html><body>" > index.html
+echo "<html><body>\n" > index.html
 for FILE in *
 do
-    echo '<a href="'$FILE'">'$FILE'</a>' >> index.html
+    echo '<a href="'$FILE'">'$FILE'</a><br>\n' >> index.html
 done
-echo "</body></html>" >> index.html
+echo "</body></html>\n" >> index.html
 
 git add .
 git commit -m "update"
