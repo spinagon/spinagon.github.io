4309736b9a469b7c313725c64640e4bf0f601a35
flak <flak.yar@gmail.com>
2020-07-03 08:47:21 +0400

    update

--- upload.sh
+++ upload.sh
@@ -1,3 +1,10 @@
+echo "<html><body>" > index.html
+for FILE in *
+do
+    echo '<a href="'$FILE'">'$FILE'</a>' >> index.html
+done
+echo "</body></html>" > index.html
+
 git add .
 git commit -m "update"
 git push -u origin master
