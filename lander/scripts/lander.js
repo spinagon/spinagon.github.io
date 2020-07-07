fec70ebe79512baa8334ed0370573f12ee599663
flak <flak.yar@gmail.com>
2020-07-07 16:17:39 +0400

    update

--- lander/scripts/lander.js
+++ lander/scripts/lander.js
@@ -15,20 +15,31 @@ function checkKey(e) {
   if (key === "d") {
     lander.rotat += 1;
   }
+  if (key === "w") {
+    lander.vy -= Math.cos(lander.rotat/360*6.28)*0.01;
+    lander.vx += Math.sin(lander.rotat/360*6.28)*0.01;
+  }
+  if (key === "s") {
+    lander.vy += 0.03;
+  }
 }
 
 lander = getLander(1);
 landerBox = document.querySelector('.lander-box');
 lander.rotat = 0;
-lander.pos = 0;
-pos = 0
+lander.posx = 50;
+lander.posy = 0;
+lander.vx = 0;
+lander.vy = 0.1;
 
 function step() {
-  lander.pos = `${pos}px`;
-  pos += 1;
+  lander.posx += lander.vx;
+  lander.posy += lander.vy;
+  lander.vy += 0.0005;
   lander.style["transform-origin"] = `50% 50%`;
   rotat(lander.rotat);
-  landerBox.style.top = lander.pos;
+  landerBox.style.top = `${lander.posy}%`;
+  landerBox.style.left = `${lander.posx}%`;
 }
 
 document.onkeydown = checkKey;
