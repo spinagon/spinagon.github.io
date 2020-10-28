a4d29370cc55b62e8db5df826901d5711bdd022f
flak <flak.yar@gmail.com>
2020-10-28 23:15:11 +0300

    up

--- anim/script.js
+++ anim/script.js
@@ -1,17 +1,16 @@
 var n = 1;
 
 function main() {
-    iframe = document.getElementById("1");
-    iframe.src = name_from_num(n);
-    frame = document.getElementById("frame");
-    page = iframe.contentDocument || iframe.contentWindow.document;
-    frame.innerHTML = page.getElementsByClassName('ascii')[0].innerHTML;
+    id = name_from_num(n);
     n += 1;
     if (n > 29) {n = 1;}
+    id_next = name_from_num(n);
+    document.getElementById(id).style = "display:none";
+    document.getElementById(id_next).style = "display:block";
     setTimeout(main, 100);
 }
 
 function name_from_num(n) {
     s = n.toLocaleString('en', {minimumIntegerDigits:4,useGrouping:false});
-    return `${s}.jpg.html`;
+    return s;
 }
